92dda7b290cb7401b656bc7de44eedb88d9229f9
Phospharadscha <95589284+Phospharadscha@users.noreply.github.com>
2022-09-28 11:38:39 +0100

    Begun commenting
    Have begun commenting the code.

--- paint_calc_gui.py
+++ paint_calc_gui.py
@@ -5,10 +5,18 @@ from enum import Enum
 from functools import total_ordering
 import math
 from tkinter import Button 
+import PySimpleGUI as sg
+import sys
 
-##### Enums #####
+#########################################################################################
+##################################### Enums #############################################
+#########################################################################################
+
 # Shapes
 class Shape(Enum):
+    """Shapes store a lambda function used to evaluate their area
+    They can then be easily calculated if I store a shape object on a wall/obstacles/etc. 
+    """
     SQUARE = lambda b: pow(b, 2)
     RECTANGLE = lambda b, h: b * h
     PARALLELOGRAM = lambda b, h: b * h
@@ -19,10 +27,16 @@ class Shape(Enum):
     SEMICIRCLE = lambda r: (math.pi * pow(r, 2)) // 2
     
     def __call__(self, args):
+        """Override the usual call method to instead allow for the supplying of a variety of multiple args.
+        These args are then passed to the matching lambda function to calculate the respective area. 
+        """
         return self.value[0](args)
     
     @classmethod
     def to_shape(self, shape_name):
+        """This is a class method, meaning it does not need to be called on a Shape object.
+        The method is passed a string, which it will then attempt to return a matching shape for.  
+        """
         match shape_name:
             case "square":
                 return Shape.SQUARE
@@ -45,7 +59,9 @@ class Shape(Enum):
 
 # Paints
 class Paint(Enum):
-    # name = [price per bucket, litres per bucket, coverage per litre]
+    """Paints are stored by their colour, with the value being the:
+    (Price per bucket, litres per bucket, coverage per litre)
+    """
     EMERALD = (24, 2.5, 13)
     SAPPHIRE = (20, 2.5, 13)
     WHITE = (45, 2.5, 12)
@@ -56,38 +72,53 @@ class Paint(Enum):
     IVORY = (10, 5, 13)
     
     def __call__(self, args):
+        """Override the usual call method. 
+        The method can be passed an argument, which will be used to return one element from the list value of the paint. 
+        """
         return self.value[args]
     
     @classmethod
     def to_paint(self, paint_name):
-        match paint_name:
-            case "red":
-                return Paint.RED
-            case "blue":
-                return Paint.BLUE
-            case "green":
-                return Paint.GREEN
-            case _:
-                return None
-            
-    def to_string(self):
-        return self.name.lower()
+        """This is a class method, meaning it does not need to be called on a Paint object.
+        The method is passed a string, which it will then attempt to return a matching paint for.  
+        """
+        # Iterates through all paints until a matching paint is found. Then returns that paint. 
+        for paint in self:
+            if paint_name.lower() == paint.name.lower():
+                return paint
+        
+        #if no match is found, then return white as a default value. 
+        return Paint.WHITE 
 
-            
+#########################################################################################
+################################### Classes #############################################
+#########################################################################################
 
-##### Classes ##### 
-# Architecture (Walls, doors, windows, etc)
 class Architecture():
+    """The parent class of Wall() and Obstacle(). 
+    An architecture object has an area- which can be calculated, and an index used to specify when asking for user input. 
+    Shape and dimensions are not stored, because they are not needed once area has been calculated. 
+    """
     def __init__(self):
         self.__surface_area = 0  
         self.__index = 0
+        self.__shape = Shape.SQUARE
 
-    def area(self):
+    @abstractmethod
+    def define(self):
+        pass
+
+    def _area(self):
         return self.__surface_area
 
     def _calc_area(self, shape, dimensions):
+        """This area is used to calculate the area of an Architecture object.  
+        It takes a provided shape (the shape of the architecture object), and its dimensions. 
+        """
+        
         surface_area = 0
         
+        # It uses the provided shape to call the lambda function assigned with it to calculate area. 
         if shape is Shape.SQUARE:
            return shape(dimensions[0])
         elif shape is Shape.RECTANGLE or shape is Shape.PARALLELOGRAM or shape is Shape.TRIANGLE:
@@ -99,20 +130,27 @@ class Architecture():
         elif shape is Shape.CIRCLE or shape is Shape.SEMICIRCLE:
            return shape(dimensions[0])
         else:
-            return wall_surface_area
+            return surface_area
 
-# A wall
 class Wall(Architecture):
+    """A wall is a child class of Architecture. 
+    Walls have additional methods to find the required number of buckets and the cost to paint the wall. 
+    """
     def __init__(self):
+        """init is overrided due to additional variables.
+        All values are assigned as a defualt and updated later. 
+        """
+        
         self.__paint = Paint.WHITE
         self.__surface_area = 0.0
         self.__coats = 1
         self.__obstacles = []
         
-    def define(self, shape, colour, num_of_obstacles):
-        import PySimpleGUI as sg
-        import sys
-        
+    def define(self, shape, colour, num_of_obstacles):  
+        """init is overrided due to additional variables.
+        All values are assigned as a defualt and updated later. 
+        """
+            
         num_of_values = 0 
         for i in range(0, num_of_obstacles):
             self.__obstacles.append(Obstacle())
@@ -223,7 +261,7 @@ class Wall(Architecture):
             self.__area_with_obstacles
         window.close()
     
-    def required_buckets(self):
+    def __required_buckets(self):
         litres_required = (self.__surface_area * self.__coats) // self.__paint(2)
         return  round(litres_required // self.__paint(1))
     
@@ -235,16 +273,11 @@ class Wall(Architecture):
     
     def __area_with_obstacles(self):
         for obstacle in self.__obstacles:
-            self.__surface_area -= obstacle.area()
-
+            self.__surface_area -= obstacle._area()
 
 # Windows, Doors, etc. 
 class Obstacle(Architecture): 
-    
-    def define(self, index, total_num):
-        import PySimpleGUI as sg
-        import sys
-        
+    def define(self, index, total_num):        
         shape_types = ["Square", "Rectangle", "Parallelogram", "Trapezoid", "Triangle", "Ellipse", "Circle", "Semicircle"]
         
         layout = [
@@ -272,13 +305,10 @@ class Obstacle(Architecture):
                     pass
         
         window.Disable()
-        self.__area(shape)   
+        self.__define_area(shape)   
         window.close()
         
-    def __area(self, shape):
-        import PySimpleGUI as sg
-        import sys
-         
+    def __define_area(self, shape):
         match shape:
             case Shape.RECTANGLE | Shape.PARALLELOGRAM | Shape.TRIANGLE:
                 num_of_values = 2 
@@ -362,7 +392,6 @@ class Obstacle(Architecture):
         self.__surface_area = self._calc_area(shape, dimensions)
         window.close()
 
-
 # Rooms
 class Room():
     def __init__(self):
@@ -383,13 +412,7 @@ class Room():
             
         return total_cost
         
-    def get_paint(self):
-        pass
-        
     def define(self, room_index):
-        import PySimpleGUI as sg
-        import sys
-        
         self.__index = room_index
         
         layout = [
@@ -424,10 +447,7 @@ class Room():
         window.close()
         
     def __populate(self): 
-        import PySimpleGUI as sg
-        import sys
-        
-        shape_types = ["Square", "Rectangle", "Parallelogram", "Trapezoid", "Triangle", "Ellipse", "Circle", "Semicircle"]
+        shapes = ["Square", "Rectangle", "Parallelogram", "Trapezoid", "Triangle", "Ellipse", "Cirlce", "Semicicle"]        
         colours = []
         for colour in Paint:   
             colours.append(colour.name.title())
@@ -440,7 +460,7 @@ class Room():
                     [sg.Text("Please enter the number of obstacles (doors/windows/etc): ")], 
                     [sg.Multiline(size=(30,1), key='obstacles')], 
                     [sg.Text("Please select the shape of the wall from the drop down menu: ")], 
-                    [sg.OptionMenu(values=shape_types,size=(30,8), default_value='Square',key='shape')],
+                    [sg.OptionMenu(values=shapes,size=(30,8), default_value='Square',key='shape')],
                     [sg.Text("Please select the colour of the wall from the drop down menu: ")], 
                     [sg.OptionMenu(values=colours,size=(30,8), default_value='White',key='colour')],
                     [sg.Button("CONFIRM")],
@@ -475,25 +495,27 @@ class Calculator():
   
         
     def main(self):
-        self.__rooms = self.__get_rooms()
+
+        while True:
+            self.__rooms = self.__get_rooms()
+
+            room_index = 1
+            for room in self.__rooms:
+                room.define(room_index)
+                room_index += 1
+
+            ## Final Screen
+            self.__final_screen()
+
         
-        room_index = 1
-        for room in self.__rooms:
-            room.define(room_index)
-            room_index += 1
-        
-        ## Final Screen    
-        self.__final_screen()
 
     def __final_screen(self): 
-        import PySimpleGUI as sg
-        import sys
-
         layout = [
             [sg.Text("All values have been entered")], 
             [sg.Button("Total Cost")],
             [sg.Button("Total Paint")],
             [sg.Button("Per Room")],
+            [sg.Button("START AGAIN")], 
             [sg.Button("CLOSE")]
         ]
         window = sg.Window("Paint Calculator", layout)
@@ -513,18 +535,16 @@ class Calculator():
                     window.Disable()
                     self.__per_room()
                     window.Enable()
+                case "START AGAIN":
+                    window.close()
+                    print("Test")
+                    break
                 case None | "CLOSE":
                     sys.exit()
-                    break
                 case _: 
                     sys.exit()
-                    break
-
-        window.close()
         
     def __total_cost(self):
-        import PySimpleGUI as sg
-        
         total_cost = 0
         for room in self.__rooms:
             for wall in room.get_walls():
@@ -545,13 +565,10 @@ class Calculator():
                     temp_window.close()
                     break
     def __total_paint(self):
-        import PySimpleGUI as sg
-        
         total_paint = {}
         
         for room in self.__rooms:
-            for wall in room.get_walls():
-                paint_colour = wall.get_paint()            
+            for wall in room.get_walls():          
                 total_paint[wall.get_paint()] = wall.required_buckets()
                 
         temp_layout = []
@@ -573,8 +590,6 @@ class Calculator():
         
 
     def __per_room(self):
-        import PySimpleGUI as sg
-        
         temp_layout = [
             [sg.Text("Please select a room: ")] 
         ]
@@ -603,8 +618,6 @@ class Calculator():
         
 
     def __room_info(self, room):
-        import PySimpleGUI as sg 
-        
         layout = [
             [sg.Text("Room: %s" % room.get_name())], 
             [sg.Button("Cost")],
@@ -668,9 +681,6 @@ class Calculator():
        
         
     def __get_rooms(self): 
-        import PySimpleGUI as sg
-        import sys
-        
         layout = [[sg.Text('Please enter the number of rooms you wish to paint in the box below:')], [sg.Multiline(size=(30,1), key='textbox')], [sg.Button("CONFIRM")], [sg.Button("CLOSE")]] 
         window = sg.Window("Paint Calculator", layout)
         
@@ -692,64 +702,41 @@ class Calculator():
         
 
 
-##### Public Functions ##### 
+#########################################################################################
+############################## Error - Handling #########################################
+#########################################################################################
+
 def get_float_input(usr_input, allow_zero):
-    import PySimpleGUI as sg
-    import sys
+    """ Returns a provided string input as a float. 
+    When called, you can specify if you want the a valid input to be non-zero, or if zero can be included. 
+    """
     
+    # Temporary assignment of values
     user_input = ""
     valid_input = False
     
+     # Loops until a valid input is given
     while True:
+        # Attempt casting of string into float
         try:
             user_input = float(usr_input)
             valid_input = True
         except ValueError:
             valid_input = False
 
+        # If the input is invalid, or if it's valid but less than 0, then:
         if not valid_input or user_input < 0:
             text = "'Error: Please enter a positive, "
+             # Alter string if zero is not allowed as a valid input
             if not allow_zero:
                 text += "non zero, "
             text += "number: "
+            
+            # Create new window asking for corrected input
             layout = [[sg.Text(text)], [sg.Multiline(size=(30,1), key='textbox')], [sg.Button("CONFIRM")], [sg.Button("CLOSE")]]
+            window = sg.Window("Error! Invalid Input", layout) 
         elif valid_input:
-            window = sg.Window("Paint Calculator") 
-            window.close()
-            return user_input
-
-        while True:
-          event, values = window.read()
-          if event == "CONFIRM":
-              usr_input = values['textbox']
-              window.close()
-              break
-          elif event == "CLOSE":
-              sys.exit()
-    
-def get_int_input(usr_input, allow_zero):
-    import PySimpleGUI as sg
-    import sys
-    
-    user_input = ""
-    valid_input = False
-    
-    while True:
-        try:
-            user_input = int(usr_input)
-            valid_input = True
-        except ValueError:
-            valid_input = False
-
-        if not valid_input or user_input < 0:
-            # append the non-zero bit here
-            text = "'Error: Please enter a positive, "
-            if not allow_zero:
-                text += "non zero, "
-            text += "whole number: "
-            layout = [[sg.Text(text)], [sg.Multiline(size=(30,1), key='textbox')], [sg.Button("CONFIRM")], [sg.Button("CLOSE")]]
-            window = sg.Window("Paint Calculator", layout) 
-        elif valid_input:
+             # Temporary window is created so that any actual window can be closed before method exits. 
             window = sg.Window("Paint Calculator") 
             window.close()
             return user_input
@@ -758,18 +745,62 @@ def get_int_input(usr_input, allow_zero):
         while True:
           event, values = window.read()
           if event == "CONFIRM":
+               # Get user input
+              usr_input = values['textbox']
+              window.close()
+              break
+          elif event == "CLOSE":
+              sys.exit()
+    
+def get_int_input(usr_input, allow_zero):
+    """ Returns a provided string input as an integer. 
+    When called, you can specify if you want the a valid input to be non-zero, or if zero can be included. 
+    """
+    
+    # Temporary assignment of values
+    user_input = ""
+    valid_input = False
+    
+    # Loops until a valid input is given
+    while True:
+        # Attempt casting of string into int 
+        try:
+            user_input = int(usr_input)
+            valid_input = True
+        except ValueError:
+            valid_input = False
+
+        # If the input is invalid, or if it's valid but less than 0, then:
+        if not valid_input or user_input < 0:
+            text = "'Error: Please enter a positive, "
+            # Alter string if zero is not allowed as a valid input
+            if not allow_zero:
+                text += "non zero, "
+            text += "whole number: "
+            
+            # Create new window asking for corrected input
+            layout = [[sg.Text(text)], [sg.Multiline(size=(30,1), key='textbox')], [sg.Button("CONFIRM")], [sg.Button("CLOSE")]]
+            window = sg.Window("Error! Invalid Input", layout) 
+        elif valid_input:
+            # Temporary window is created so that any actual window can be closed before method exits. 
+            window = sg.Window("Paint Calculator") 
+            window.close()
+            return user_input
+
+        window.refresh()
+        while True:
+          event, values = window.read()
+          if event == "CONFIRM":
+              # Get user input
               usr_input = values['textbox']
               window.close()
               break
           elif event == "CLOSE":
               sys.exit()
 
-
-def clear_console():
-    import os
-    clear = lambda: os.system('clear')
-
 if __name__ == '__main__':
+    # All of the program is run through the Calculator class
+    # Create a calculator object, and then call its main() function
     calculator = Calculator()
     calculator.main()
     
